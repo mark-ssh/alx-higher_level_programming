9cf608698e1eed0b460153006e57f2ebd4f2f132
Mark Wonah <markwonah@gmail.com>
2023-05-30 06:35:27 -0700

    updated tasks to pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
 
-     items = 0
-     try:
+    items = 0
+    try:
         for elements in range(0, x):
             print("{}".format(my_list[elements]), end="")
             items += 1
-     except IndexError:
-         pass
-     print()
-     return items
+    except IndexError:
+        pass
+    print()
+    return items
