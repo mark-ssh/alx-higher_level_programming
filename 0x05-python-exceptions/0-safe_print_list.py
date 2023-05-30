f80decfdb34214334ba3dc53f12b3a2816c346c1
Mark Wonah <markwonah@gmail.com>
2023-05-30 06:10:25 -0700

    prints x element of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+
+     items = 0
+     try:
+        for elements in range(0, x):
+            print("{}".format(my_list[elements]), end="")
+            items += 1
+     except IndexError:
+         pass
+     print()
+     return items
