a8e91866676fef7cf2b6b6c965b250e12ea32630
Mark Wonah <markwonah@gmail.com>
2023-05-31 07:35:29 -0700

    class square based on task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py
+# Mark Wonah
+"""Define a class Square."""
+
+
+class Square:
+    """Class of a square."""
+    def __init__(self, size):
+        """__init__
+        This initializes the size value of the square.
+        Attributes:
+            size (int): The size of the square.
+        """
+        self.__size = size
