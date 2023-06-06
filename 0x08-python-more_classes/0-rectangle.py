2ccdfdd3aacb4cf5ac9676e131ce47d2fb211750
Mark Wonah <markwonah@gmail.com>
2023-06-06 04:06:52 -0700

    an empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Mark Wonah
+"""
+Simple rectangle : An empty class rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An empty rectangle class
+    """
+
+    pass
