3418ec7c0fc4a1d09f4d3db4b638a5f1bff3c562
Mark Wonah <markwonah@gmail.com>
2023-06-08 07:18:57 -0700

    update to task 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -23,9 +23,8 @@ def print_square(size):
     if size < 0:
         raise ValueError('size must be >= 0')
 
-     for i in range(1, size + 1):
-         for j in range(1, size + 1):
-             print('#', end='')
-
-             if j % size == 0 and j > 0:
-                 print()
+    for i in range(1, size + 1):
+        for j in range(1, size + 1):
+            print('#', end='')
+    if j % size == 0 and j > 0:
+        print()
